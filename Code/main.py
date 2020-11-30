78823f6f19240c7ca979170edf482d082d6def4b
Miguel Angel Velazquez Romero <miguel.program.73@gmail.com>
2020-11-30 00:15:40 -0600

    15.-Flask Bootstrap

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app) 
+
 
 todos = ['Comprar cafe', 'Enviar una solicitud de compra', 'Entregar video de producto']
 
